1164051ec554017ca76dcdb4d3fb46ea078f2704
reddyAnkitha <140939251+reddyAnkitha@users.noreply.github.com>
2026-07-03 16:31:40 +0530

    Update app and documentation

--- app.py
+++ app.py
@@ -15,7 +15,13 @@ from utils.pdf_generator import generate_pdf
 # LOAD API KEY
 # ===========================
 load_dotenv()
-genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
+
+api_key = os.getenv("GEMINI_API_KEY")
+
+if not api_key:
+    raise ValueError("GEMINI_API_KEY not found in .env file")
+
+genai.configure(api_key=api_key)
 
 model = genai.GenerativeModel("gemini-2.5-flash")
 
